3fef3f9b67619f557ba4cc4673ba7f8df4f30130
wglee0511 <ranazang@naver.com>
2021-03-12 16:02:33 +0900

    Plus and Minus button just use vanilla javascript

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const add = document.querySelector(".add");
+const minus = document.querySelector(".minus");
+const span = document.querySelector("span");
+
+let count = 0;
+
+span.innerText = count;
+const updateText = () => {
+  span.innerText = count;
+}
+
+const handleAdd = () => {
+  count = count + 1;
+  updateText();
+}
+const handleMinus = () => {
+  count = count - 1;
+  updateText();
+}
+
+const init = () => {
+  add.addEventListener("click", handleAdd);
+  minus.addEventListener("click", handleMinus);
+}
+
+init()

dbe21c8e580792a131cfa74d3a2eee976c7d9bbb
wglee0511 <ranazang@naver.com>
2021-03-15 03:02:13 +0000

    code arrangement

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { actionCreator, delToDo } from "../store";
+import { delToDo } from "../store";
 
 function ToDo(props) {
     const {text, id ,delToDo} = props;
